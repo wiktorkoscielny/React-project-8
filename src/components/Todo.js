05b2b85af55e88128dbaf29e1f630875dca2029e
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-20 21:27:48 +0200

    Posting new state to json, setting new state

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -32,10 +32,57 @@ export class Todo extends Component {
         this.fetchTodo()
     }
 
+    // changing state value
+    handleChange = (e, name) => {
+        console.log(e.target.value);
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+
+    // posting state value to json, setting new state
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // Save data on json
+        axios.post(`https://my-json-server.typicode.com/wiktorkoscielny/React-project-8/todos`, {
+          firstName: this.state.state1,
+        })
+          .then(res => {
+            // console.log(res.data);
+            this.setState({
+              state1: '',
+              todos: [
+                ...this.state.todos,
+                {
+                  firstName: res.data.firstName,
+                  id: res.data.id
+                }
+              ]
+            })
+        })
+    }
+
     render() {
         return (
             <div className='container'>
                 
+                {/* FORM */}
+
+                <div className='container__label'>
+                        <form
+                            onSubmit={(e) => this.handleSubmit(e)}
+                        >
+                            <label>First Name:</label>
+                            <input
+                                type='text'
+                                value={this.state.state1}
+                                onChange={(e) => this.handleChange(e, 'state1')}
+                            >
+                            </input>
+                            <button type='submit'>Add</button>
+                        </form>
+                </div>
+
 
                 {this.state.todos.map((todo) => {
                     return(

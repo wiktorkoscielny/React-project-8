bd39f3a11d4faab4c5e037e4f821c63b160be62a
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-20 20:56:40 +0200

    Initial commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 
 import axios from 'axios'
 
-// JSONbin with JSON
-const getUrl = 'https://api.jsonbin.io/b/62b098155c2a444a2d928f10'; // request URL
-const secretKey = '$2b$10$/NTPoz.KvyNk4pCk4BUZgurt2vQkkWd44q9REC.DuULvvXPpUPyJ2' // access token
-
 export class Todo extends Component {
     state = {
         state1: '',
@@ -37,61 +33,12 @@ export class Todo extends Component {
         this.fetchTodo()
       }
 
-// changing state value
-handleOnChange = (e, name) => {
-    console.log(e.target.value);
-    this.setState({
-        [name]: e.target.value
-    })
-}
 
-// posting new state
-handleSubmit = async (e) => {
-    e.preventDefault();
-    // saving data on JSONbin
-    await axios.put(getUrl, {
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Master-Key': secretKey,
-        },
-    }, {
-        firstName: this.state.state1,
-    }).then(res => {
-        console.log(res);
-        this.setState({
-            state1: '',
-            todos: [
-               ...this.state.todos,
-                {
-                    firstName: res.data.firstName,
-                }
-            ]
-        });
-    })
-}
-
-componentDidUpdate(prevProps) {
-
-}
 
     render() {
         return (
             <div className='container'>
-                <div className='container__label'>
-                    <form
-                        onSubmit={(e) => this.handleSubmit(e)}
-                    >
-                        <label>First Name:</label>
-                        <input
-                            type='text'
-                            value={this.state.state1}
-                            onChange={(e) => this.handleOnChange(e, 'state1')}
-                        >
-                        </input>
-
-                        <button type='submit'>Add</button>
-                    </form>
-                </div>
+                
 
                 {this.state.todos.map((todo) => {
                     return(

64b80af8ce86ec72688fb082b568f297ea699b28
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-27 13:38:13 +0200

    Created styles for TodoList component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,50 +1,60 @@
 import React from 'react'
 
+// styles
+import styled from "styled-components";
+
+const TableWrapper = styled.div`
+        width: fit-content;
+        text-align: center;
+        margin-top: 3%;
+        margin-left: auto;
+        margin-right: auto;
+`
+
 export const TodoList = (props) => {
 
   return (
     <>
-    
-        <table border='3'>
-                <caption>Users</caption>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Salary</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.todos.map((todo, index) => {
-                        return(
-                            <tr key={index}>
-                                <td>{todo.firstName}</td>
-                                <td>{todo.lastName}</td>
-                                <td>{todo.salary}</td>
-                                {/* DELETE */}
-                                <td>
-                                    <button
-                                        onClick={(event) => props.removeTodo(todo.id)}
-                                    >
-                                        X
-                                    </button>
-                                </td>
+        <section>
+            <TableWrapper>
+                <table border='3'>
+                        <caption>Users</caption>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Salary</th>
+                                <th>Delete</th>
                             </tr>
-                        )
-                    })}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan="2">Summary</td>
-                        <td colSpan="2">{props.sumOfSalary}</td>
-                    </tr>
-                    <tr>
-                        <td colSpan="4">Delete All<button>X</button></td>
-                    </tr>
-                </tfoot>
-        </table>
-        
+                        </thead>
+                        <tbody>
+                            {props.todos.map((todo, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td>{todo.firstName}</td>
+                                        <td>{todo.lastName}</td>
+                                        <td>{todo.salary}</td>
+                                        {/* DELETE */}
+                                        <td>
+                                            <button
+                                                onClick={(event) => props.removeTodo(todo.id)}
+                                            >
+                                                X
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colSpan="2">Summary</td>
+                                <td colSpan="2">{props.sumOfSalary}</td>
+                            </tr>
+                        </tfoot>
+                </table>
+            </TableWrapper>
+        </section>
     </>
   )
 }

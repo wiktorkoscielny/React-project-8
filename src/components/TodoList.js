7c6f2be776b70611a4349876512d584028e8734d
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-27 11:39:14 +0200

    Created table to arrange data into rows and columns

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,50 @@
 import React from 'react'
 
 export const TodoList = (props) => {
+
   return (
     <>
-        {props.todos.map((todo, index) => {
-                    return(
-                        <ul key={index}>
-                            <li>{todo.firstName}</li>
-                            {/* DELETE */}
-                            <button
-                                onClick={(event) => props.removeTodo(todo.id)}
-                            >
-                                X
-                            </button>
-                        </ul>
-                    )
-                })}
+    
+        <table border='3'>
+                <caption>Users</caption>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Salary</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.todos.map((todo, index) => {
+                        return(
+                            <tr key={index}>
+                                <td>{todo.firstName}</td>
+                                <td>{todo.lastName}</td>
+                                <td>{todo.salary}</td>
+                                {/* DELETE */}
+                                <td>
+                                    <button
+                                        onClick={(event) => props.removeTodo(todo.id)}
+                                    >
+                                        X
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan="2">Summary</td>
+                        <td colSpan="2">{props.sumOfSalary}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4">Delete All<button>X</button></td>
+                    </tr>
+                </tfoot>
+        </table>
+        
     </>
   )
 }

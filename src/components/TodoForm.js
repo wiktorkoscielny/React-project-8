c1167cf692673ce9813c8e93923980f7fd1b25e3
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-06-27 11:38:37 +0200

    Created 2 new inputs to enter values to state2 and state3

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,13 +8,29 @@ export const TodoForm = (props) => {
             <form
                 onSubmit={(e) => props.handleSubmit(e)}
             >
-                <label>First Name:</label>
-                <input
-                    type='text'
-                    value={props.state1}
-                    onChange={(e) => props.handleChange(e, 'state1')}
-                >
-                </input>
+                    <label>First Name:</label>
+                    <input
+                        type='text'
+                        value={props.state1}
+                        onChange={(e) => props.handleChange(e, 'state1')}
+                    >
+                    </input>
+
+                    <label>Last Name:</label>
+                    <input
+                        type='text'
+                        value={props.state2}
+                        onChange={(e) => props.handleChange(e, 'state2')}
+                    >
+                    </input>
+
+                    <label>Salary:</label>
+                    <input
+                        type='number'
+                        value={props.state3}
+                        onChange={(e) => props.handleChange(e, 'state3')}
+                    >
+                    </input>
                 <button type='submit'>Add</button>
             </form>
         </div>
